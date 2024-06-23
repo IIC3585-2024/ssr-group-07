27a2472ae1fa10be7ad05b8ec99195c3c7c26666
Jimnora <djimnez@uc.cl>
2024-06-22 20:05:33 -0400

    feat: Add TMDB API integration for series information

---
+++ src/lib/api/getSeriesbyName.js
@@ -0,0 +1,26 @@
+export default async function getSeriesByName(name) {
+    const token = process.env.NEXT_PUBLIC_TMDB_TOKEN
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+        }
+    }
+    const response = await fetch(`https://api.themoviedb.org/3/search/tv?query=${name}&language=en-US`, options)
+    const data = await response.json()
+
+    // get genres
+    const genres = await fetch(`https://api.themoviedb.org/3/genre/tv/list`, options)
+    const genreList = await genres.json()
+
+    // add genre names to series
+    data.results.forEach(series => {
+        series.genre_names = series.genre_ids.map(id => {
+            const genre = genreList.genres.find(genre => genre.id === id)
+            return genre.name
+        }).join(", ")
+    })
+    
+    return data.results
+}
