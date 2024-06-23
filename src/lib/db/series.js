c185e7e21418c2a05994d26c29c7481b8ccc4a6f
Jimnora <djimnez@uc.cl>
2024-06-22 20:06:30 -0400

    feat: Refactor series database functions and add comment functionality

--- src/lib/db/series.js
+++ src/lib/db/series.js
@@ -1,20 +1,49 @@
 'use server'
 import { db } from "../firebase/config";
+import { collection, addDoc, getDocs, getDoc, doc, updateDoc, arrayUnion } from "firebase/firestore";
 
 export const fetchSeries = async (id) => {
-    const doc = await db.collection("series").doc(id).get();
-    setSeries(doc.data());
-    setLoading(false);
+    const docRef = doc(db, "series", id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        return docSnap.data();
+    } else {
+        return null;
+    }
 }
 export const fetchComments = async (id) => {
-    const snapshot = await db.collection("comments").where("seriesId", "==", id).get();
+    const query = collection(db, "comments");
+    const snapshot = await getDocs(query);
     const data = snapshot.docs.map(doc => doc.data());
-    setComments(data);
+    return data;
 }
+
 export const addComment = async (id, comment, rating) => {
-    await db.collection("comments").add({
+    await addDoc(collection(db, "comments"), {
         seriesId: id,
-        content: comment,
+        comment,
         rating
+    });
+
+    // change rating of series
+    const seriesRef = doc(db, "series", id);
+    const seriesDoc = await getDoc(seriesRef);
+    const seriesData = seriesDoc.data();
+    const newRating = (seriesData.rating * seriesData.rating_count + rating) / (seriesData.rating_count + 1);
+    const newRatingCount = seriesData.rating_count + 1;
+    await updateDoc(seriesRef, {
+        rating: newRating,
+        rating_count: newRatingCount
+    });
+}
+
+export const addSeries = async (seriesId, title, rating, rating_count, description, genres) => {
+    await addDoc(collection(db, "series"), {
+        seriesId,
+        title,
+        rating,
+        rating_count,
+        description,
+        genres
     });
 }
