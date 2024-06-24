eef2924de08a383e6115ecea75b092f1a4823612
Jimnora <djimnez@uc.cl>
2024-06-24 04:53:45 -0400

    refactor: Add loading state to Header component

--- src/app/header.js
+++ src/app/header.js
@@ -2,35 +2,40 @@ import Link from 'next/link';
 import { useAuth } from './context/auth';
 import { logOut } from '@/lib/firebase/auth';
 import styles from './header.module.css'
+import { Fragment, useEffect, useState } from 'react';
 
 export default function Header() {
     const { currentUser } = useAuth();
-    
-    return (
-        <header>
-            {currentUser ? (
-            <div className={styles.navbar}>
-                <div className={styles.navtabs}>
-                    <Link className={styles.navtab} href="/">Home</Link>
-                    
-                    <Link className={styles.navtab} href="/search">Search</Link>
-                
-                    <Link className={styles.navtab} href="/add-series">Add Serie</Link>
-                
-                    <span className={styles.navtab} onClick={() => logOut()}>Logout</span>
-                </div>
-            </div>    
-            ) : (
-            <div className={styles.navbar}>
-                <div className={styles.navtabs}>
-                    <Link className={styles.navtab} href="/">Home</Link>
+    const [ loading, setLoading ] = useState(true);
 
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    function headerHandler() {
+        if (currentUser && !loading) {
+            return (
+                    <span className={styles.navtab} onClick={() => logOut()}>Logout</span>
+            )
+        } else if (!loading) {
+            return (
+                <Fragment>
                     <Link className={styles.navtab} href="/login">Login</Link>
 
                     <Link className={styles.navtab} href="/signup">Sign Up</Link>
+                </Fragment>
+            )
+        }
+    }
+    
+    return (
+        <header>
+            <div className={styles.navbar}>
+                <div className={styles.navtabs}>
+                    <Link className={styles.navtab} href="/">Home</Link>
+                    {headerHandler()}
                 </div>
             </div>
-            )}
         </header>
     );
 }
