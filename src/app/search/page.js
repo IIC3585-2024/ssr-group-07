2c8f91b444f8f639d403c337479d12a738dec55e
Jimnora <djimnez@uc.cl>
2024-06-24 00:47:31 -0400

    feat: get genres and providers list

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,8 +1,10 @@
 'use client'
 import Link from 'next/link';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { search } from "@/lib/db/search";
 import SeriesCard from "./seriesCard";
+import getGenres from '@/lib/api/getGenres';
+import getProviders from '@/lib/api/getProviders';
 
 export default function SearchPage() {
     const [text, setText] = useState("");
@@ -11,6 +13,8 @@ export default function SearchPage() {
     const [order, setOrder] = useState("desc");
     const [errorMessage, setErrorMessage] = useState("");
     const [results, setResults] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const [providers, setProviders] = useState([]);
 
     const clientSearch = () => {
         if (!text && !rating && !order) {
@@ -28,16 +32,50 @@ export default function SearchPage() {
         });
     }
 
-    return (
-        <main>
-            <h1>Search</h1>
-            <input
+    useEffect(() => {
+        getGenres().then((res) => {
+            setGenres(res);
+        });
+        getProviders().then((res) => {
+            setProviders(res);
+        });
+    }, []);
+
+    function showSpecificSearch() {
+        if (queryField === 'title') {
+            return (
+                <input
                 className="input-text-field margin-5"
                 type="text"
                 placeholder="Search"
                 value={text}
                 onChange={e => setText(e.target.value)}
             />
+            )
+        } else if (queryField === 'genre' && genres.length > 0) {
+            return (
+                <select className="input-dropdown">
+                    {genres.map((genre, index) => (
+                        <option key={index} value={genre.name}>{genre.name}</option>
+                    ))}
+                </select>
+            )
+        } else if (queryField === 'service' && providers.length > 0) {
+            return (
+                <select className="input-dropdown">
+                    {providers.map((provider, index) => (
+                        <option key={index} value={provider.provider_name}>{provider.provider_name}</option>
+                    ))}
+                </select>
+            )
+        }
+    }
+
+
+    return (
+        <main>
+            <h1>Search</h1>
+            {showSpecificSearch()}
             <select className="input-dropdown" onChange={e => setQueryField(e.target.value)}>
                 <option value="title">Title</option>
                 <option value="genre">Genre</option>
