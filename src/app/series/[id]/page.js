1da46930ce15f1b467757a4f2496d5d53fe4a4d4
Pedro Palma <pedro.palma@uc.cl>
2024-06-25 23:45:02 -0400

    feat: Change img to Image in series page

--- src/app/series/[id]/page.js
+++ src/app/series/[id]/page.js
@@ -4,11 +4,10 @@ import { useState, useEffect } from 'react';
 import { fetchSeries, fetchComments, addComment } from '@/lib/db/series';
 import { useAuth } from '../../context/auth';
 import getProvidersById from '@/lib/api/getProvidersById';
-
-
 import styles from './page.module.css';
 import getSeriesInfo from '@/lib/api/getSeriesInfo';
 import SeasonTable from '@/app/components/SeasonTable';
+import Image from 'next/image';
 
 export default function SeriesPage() {
     const { currentUser } = useAuth();
@@ -86,79 +85,79 @@ export default function SeriesPage() {
                     {!series || !series.title ? (
                         <p>Series not found</p>
                     ) : (
-<div className={styles["serie-detail-container"]}>
-    <div className={styles.container}>
-        <img className={styles.cover} src={`https://image.tmdb.org/t/p/w500${series.poster_path}`} alt="book"/>
-        <div>
-            <h1 className={styles.title} >{series.title}</h1>
-            <p>Genres: {series.genres.join(', ')}</p>
-            <div className={styles["ratings-container"]}>
-            <ul className={styles["ratings"]}>
-                <li className={styles["rating-avg"]}>
-                {renderStars(series.rating)} <span className={styles["average"]}>{series.rating.toFixed(1)}</span>
-                </li>
-            </ul>
-            <p>{series.rating_count} Ratings</p>
-            </div>
-            <div className={styles["providers-section"]}>
-                <h4>Watch Providers</h4>
-                {providers.length > 0 ? (
-                    <ul className={styles["providers-list"]}>
-                        {providers.map((provider, index) => (
-                            <li key={index} className={styles["provider-item"]}>
-                                <img className={styles["providers-img"]} src={`https://image.tmdb.org/t/p/w200${provider.logo_path}`} alt={provider.provider_name} />
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No providers available</p>
-                )}
-            </div>
-            <p className={styles["description"]}>{series.description}</p>
-            <SeasonTable seasons={seasons} />
-        </div>
-    </div>
-    <div className={styles["comments-section"]}>
-        <h2 className={styles.title}>Comments</h2>
-        
-        <div className={styles["comment-form"]}>
-            <input
-                type="number"
-                className="input-text-field margin-5"
-                placeholder="Rating"
-                value={rating}
-                onChange={e => setRating(parseInt(e.target.value))}
-            />
-            <textarea
-                type="text"
-                className={`input-text-field margin-5 ${styles["textarea"]}`}
-                placeholder="Comment"
-                value={comment}
-                onChange={e => setComment(e.target.value)}
-            />
-            <button className="btn margin-5" onClick={createComment}>Add Comment</button>
-        </div>
+                        <div className={styles["serie-detail-container"]}>
+                            <div className={styles.container}>
+                                <Image className={styles.cover} src={`https://image.tmdb.org/t/p/w500${series.poster_path}`} alt="book" width={260} height={400}/>
+                                <div>
+                                    <h1 className={styles.title} >{series.title}</h1>
+                                    <p>Genres: {series.genres.join(', ')}</p>
+                                    <div className={styles["ratings-container"]}>
+                                    <ul className={styles["ratings"]}>
+                                        <li className={styles["rating-avg"]}>
+                                        {renderStars(series.rating)} <span className={styles["average"]}>{series.rating.toFixed(1)}</span>
+                                        </li>
+                                    </ul>
+                                    <p>{series.rating_count} Ratings</p>
+                                    </div>
+                                    <div className={styles["providers-section"]}>
+                                        <h4>Watch Providers</h4>
+                                        {providers.length > 0 ? (
+                                            <ul className={styles["providers-list"]}>
+                                                {providers.map((provider, index) => (
+                                                    <li key={index} className={styles["provider-item"]}>
+                                                        <Image className={styles["providers-img"]} src={`https://image.tmdb.org/t/p/w200${provider.logo_path}`} alt={provider.provider_name} width={32} height={32}/>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        ) : (
+                                            <p>No providers available</p>
+                                        )}
+                                    </div>
+                                    <p className={styles["description"]}>{series.description}</p>
+                                    <SeasonTable seasons={seasons} />
+                                </div>
+                            </div>
+                            <div className={styles["comments-section"]}>
+                                <h2 className={styles.title}>Comments</h2>
+                                
+                                <div className={styles["comment-form"]}>
+                                    <input
+                                        type="number"
+                                        className="input-text-field margin-5"
+                                        placeholder="Rating"
+                                        value={rating}
+                                        onChange={e => setRating(parseInt(e.target.value))}
+                                    />
+                                    <textarea
+                                        type="text"
+                                        className={`input-text-field margin-5 ${styles["textarea"]}`}
+                                        placeholder="Comment"
+                                        value={comment}
+                                        onChange={e => setComment(e.target.value)}
+                                    />
+                                    <button className="btn margin-5" onClick={createComment}>Add Comment</button>
+                                </div>
 
-        <div className={styles["comments-container"]}>
-            {comments && comments.map((comment, index) => (
-                <div key={index} className={styles["comment-item"]}>
-                    <div className={styles["comment-content"]}>
-                        <div className={styles["comment-meta"]}>
-                            <h3 className={styles["comment-by"]}>{comment.username}</h3>
-                            <span className={styles["comment-rating"]}>{renderStars(comment.rating)}</span>
+                                <div className={styles["comments-container"]}>
+                                    {comments && comments.map((comment, index) => (
+                                        <div key={index} className={styles["comment-item"]}>
+                                            <div className={styles["comment-content"]}>
+                                                <div className={styles["comment-meta"]}>
+                                                    <h3 className={styles["comment-by"]}>{comment.username}</h3>
+                                                    <span className={styles["comment-rating"]}>{renderStars(comment.rating)}</span>
+                                                </div>
+                                                <p className={styles["comment-text"]}>{comment.comment}</p>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+
+                            </div>
                         </div>
-                        <p className={styles["comment-text"]}>{comment.comment}</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-
-    </div>
-</div>
 
                     )}
                 </>
             )}
-        </main>
+    </main>
     );
 }
